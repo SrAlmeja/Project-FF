65cfa809b8ae951b52ebd988b0c2446b539f6585
Inquisidor <cdmx1895@amerike.edu.mx>
2022-10-03 17:00:09 -0500

    Intentando avanzar con Yodo

---
+++ Project FF/Assets/AlmejaStudios/Code/New Code/Systems/YodoCode/AddsManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yodo1.MAS;
+
+public class AddsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Yodo1AdBuildConfig config = new Yodo1AdBuildConfig().enableUserPrivacyDialog(true);
+        Yodo1U3dMas.SetAdBuildConfig(config);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project FF/Assets/AlmejaStudios/Code/New Code/Systems/YodoCode/PrivacyMessage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yodo1.MAS;
+
+public class PrivacyMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Yodo1MasUserPrivacyConfig userPrivacyConfig = new Yodo1MasUserPrivacyConfig()
+            .titleBackgroundColor(Color.green)
+            .titleTextColor(Color.blue)
+            .contentBackgroundColor(Color.black)
+            .contentTextColor(Color.white)
+            .buttonBackgroundColor(Color.red)
+            .buttonTextColor(Color.green);
+
+        Yodo1AdBuildConfig config = new Yodo1AdBuildConfig()
+            .enableUserPrivacyDialog(true)
+            .userPrivacyConfig(userPrivacyConfig);
+
+        Yodo1U3dMas.SetAdBuildConfig(config);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
