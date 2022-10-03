051ea5b45b8bf3fabf2250fe1b8413155099cc31
Angel Legorreta <legosurio@hotmail.com>
2022-10-03 13:43:26 -0500

    Pausa

--- Project FF/Assets/AlmejaStudios/Code/Old Code/Core GameLogic/SceneTransition.cs
+++ Project FF/Assets/AlmejaStudios/Code/Old Code/Core GameLogic/SceneTransition.cs
@@ -8,6 +8,17 @@ public class SceneTransition : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(SceneToLoad);
     }
+    
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+    
     public void Quit()
     {
         Application.Quit();
