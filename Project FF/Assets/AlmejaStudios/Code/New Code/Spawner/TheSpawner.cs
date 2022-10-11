7f5a04614e9ae275f38bd80546b2321984fac215
Sr Almeja <cdmx1895@amerike.edu.mx>
2022-10-11 18:47:10 -0500

    Empezando a optimizar y reparar errores

---
+++ Project FF/Assets/AlmejaStudios/Code/New Code/Spawner/TheSpawner.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject _points;
+    [SerializeField] private GameObject _normalMap;
+    [SerializeField] private GameObject _addsMap;
+    [SerializeField] private GameObject _brokenMap;
+    [SerializeField] private GameObject _brokenMap2;
+
+    private void Awake()
+    {
+        // if()
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+}
